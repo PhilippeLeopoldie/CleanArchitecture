681791d86c5d8df0660ad019bdee8c58b35f7bff
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-05-26 15:04:35 +0200

    Refactor EmployeeRepository using directives
    Added using directive for EmployeesApp.Domain.Models. Removed unnecessary using directives to streamline code dependencies.

--- EmployeesApp.Infrastructure/Persistence/Repositories/EmployeeRepository.cs
+++ EmployeesApp.Infrastructure/Persistence/Repositories/EmployeeRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using EmployeesApp.Application;
+﻿using EmployeesApp.Application;
 using EmployeesApp.Domain.Models;
 
 namespace EmployeesApp.Infrastructure.Persistence.Repositories;
